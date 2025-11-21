78f434bcc1330a9cd3dfb1acea5d48bcad9652a4
tumbiko <vitumbikoshaba0@gmail.com>
2025-11-21 16:12:29 +0200

    gghfg yujh yuh

--- app/api/test/route.ts
+++ app/api/test/route.ts
@@ -1,5 +1,17 @@
+import { writeClient } from "@/sanity/lib/writeclient";
+import { NextResponse } from "next/server";
+
 export async function GET() {
-  console.log("PROJECT_ID =>", process.env.PROJECT_ID);
-  console.log("DATASET =>", process.env.DATASET);
-  return Response.json({ ok: true });
+  try {
+    const doc = await writeClient.create({
+      _type: "address",
+      firstName: "Test",
+      lastName: "TokenCheck",
+    });
+    
+    return NextResponse.json({ ok: true, doc });
+  } catch (err: unknown) {
+    const message = err instanceof Error ? err.message : String(err);
+    return NextResponse.json({ ok: false, error: message });
+  }
 }
