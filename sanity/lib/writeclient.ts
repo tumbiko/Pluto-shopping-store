1efe172177b49367d1ffbe76da111d73cf90720c
tumbiko <vitumbikoshaba0@gmail.com>
2025-11-23 20:41:11 +0200

    write issues in sanity fix

--- sanity/lib/writeclient.ts
+++ sanity/lib/writeclient.ts
@@ -1,19 +1,9 @@
 import { createClient } from "next-sanity";
 
-if (!process.env.NEXT_PUBLIC_SANITY_PROJECT_ID) {
-  console.warn("Missing NEXT_PUBLIC_SANITY_PROJECT_ID");
-}
-if (!process.env.NEXT_PUBLIC_SANITY_DATASET) {
-  console.warn("Missing NEXT_PUBLIC_SANITY_DATASET");
-}
-if (!process.env.SANITY_WRITE_TOKEN) {
-  console.warn("Missing SANITY_WRITE_TOKEN");
-}
-
 export const writeClient = createClient({
-  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || "",
-  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || "",
+  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
+  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET,
   apiVersion: "2023-10-01",
-  token: process.env.SANITY_WRITE_TOKEN || "",
+  token: process.env.SANITY_API_WRITE_TOKEN || "", // avoid crash
   useCdn: false,
 });
