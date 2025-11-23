679e540ca9de3db52748beba83032e9b6740a63a
tumbiko <vitumbikoshaba0@gmail.com>
2025-11-23 20:16:29 +0200

    env var issue fix

--- sanity/lib/client.ts
+++ sanity/lib/client.ts
@@ -1,9 +1,8 @@
-import { createClient } from 'next-sanity'
+import { createClient } from "next-sanity";
 
 export const client = createClient({
-  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!,
-  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET!,
+  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
+  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET,
   apiVersion: "2023-01-01",
-  useCdn: false,
-  token: process.env.SANITY_WRITE_TOKEN, // optional
-})
+  useCdn: true,
+});
--- sanity/lib/writeclient.ts
+++ sanity/lib/writeclient.ts
@@ -1,9 +1,9 @@
 import { createClient } from "next-sanity";
 
 export const writeClient = createClient({
-  projectId: process.env.PROJECT_ID!,
-  dataset: process.env.DATASET!,
+  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
+  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET,
   apiVersion: "2023-10-01",
-  token: process.env.SANITY_API_WRITE_TOKEN!,
+  token: process.env.SANITY_WRITE_TOKEN || "", // avoid crash
   useCdn: false,
 });
